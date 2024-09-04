1b90207c8086b754d9e99c00df3ec06ce7255ffa
Jemzkie <jemrowynn506@gmail.com>
2024-09-04 20:13:49 +0800

    Calculator

---
+++ app/src/main/java/com/rosal/calculator/MainActivity.java
@@ -0,0 +1,94 @@
+package com.rosal.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText number1EditText, number2EditText;
+    private TextView resultTextView;
+    private Button addButton, subtractButton, multiplyButton, divideButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1EditText = findViewById(R.id.number1EditText);
+        number2EditText = findViewById(R.id.number2EditText);
+        resultTextView = findViewById(R.id.resultTextView);
+        addButton = findViewById(R.id.addButton);
+        subtractButton = findViewById(R.id.subtractButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        divideButton = findViewById(R.id.divideButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('+');
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('-');
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('*');
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate('/');
+            }
+        });
+    }
+
+    private void calculate(char operator) {
+        String num1Str = number1EditText.getText().toString();
+        String num2Str = number2EditText.getText().toString();
+
+        if (!num1Str.isEmpty() && !num2Str.isEmpty()) {
+            double num1 = Double.parseDouble(num1Str);
+            double num2 = Double.parseDouble(num2Str);
+            double result = 0;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        resultTextView.setText("Error");
+                        return;
+                    }
+                    break;
+            }
+
+            DecimalFormat df = new DecimalFormat("0.00");
+            resultTextView.setText(df.format(result));
+        } else {
+            resultTextView.setText("0.00");
+        }
+    }
+}
